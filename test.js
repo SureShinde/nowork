47640a42bc98161a6e89469c83381f7229d87b1c
rrmodi88 <rrmodi@gmail.com>
2014-09-03 16:00:51 +0530

    Second Attempt WIP

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 jQuery(document).delegate("#tab2","click",function() {
 
-	jQuery(".lazy").lazyload({
-	    threshold : 200
-	});
+	 //aa char ma thi 
+	 //$(window).lazyLoadXT();
+	 //$.lazyLoadXT.selector
+	 //$(elements).lazyLoadXT();
+	 JQuery("YOUR_CONTAINER_ID_OR_CLASS").find("TAG_OR_CLASS_ON_WHICH_YOU_WANT_TO_APPLY_LAZYLOAD").lazyLoadXT();
 
 })
