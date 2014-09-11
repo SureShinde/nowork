bdf593b53fce850707ce3877765ab0564f820e62
rrmodi88 <rrmodi@gmail.com>
2014-09-11 19:58:41 +0530

    mobile detection

---
+++ mobiledetect.js
@@ -0,0 +1,15 @@
+//use this in script tag in head part//
+
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+ 	//   return true;
+	//put responsive css and js over here
+	//by putting this there will be no responsiveness on the desktop
+  }
+ 
